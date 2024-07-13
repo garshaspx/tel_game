127ad600e9c46f0c0a6b4f3d78b81a8aa8150ef7
amirhosein24 <am.heidari24@gmail.com>
2024-07-13 16:40:45 +0330

    update

--- static/logic/pages/profile.mjs
+++ static/logic/pages/profile.mjs
@@ -4,10 +4,10 @@ export function loadProfilePage(userid) {
     return `
         <div class="profile-container">
             <div class="profile-image">
-                <img src='../static/imgs/${userid}.png' alt="Profile Image">
+                <img src='static/imgs/MTdmNjg5M2QtZDg5NS00ZDk5LWI5OWYtNmY2YWY3ZTgzOGQ0.png' alt="Profile Image">
             </div>
             <div class="profile-header">
-                <img src='../static/imgs/1234.png' alt="header">
+                <img src='static/imgs/1234.png' alt="header">
             </div>
             <h2 class="profile-username">${userid}</h2>
             <a href="http://t.me/garshaspx" class="profile-username" target="_blank">
